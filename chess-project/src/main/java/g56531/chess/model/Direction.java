9ea0fc3a27cf005e8ecf131be829f3ad7a992bdb
56531 <56531@etu.he2b.be>
2022-03-15 14:18:37 +0100

    Créer l'enumeration Direction

---
+++ chess-project/src/main/java/g56531/chess/model/Direction.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package g56531.chess.model;
+
+/**
+ * Gives a direction taken by the pawn
+ * @author larsi
+ */
+public enum Direction {
+  NW(1,-1), N(1,0), NE(1,1), W(0,-1), E(0,1), SW(-1,-1), S(-1,0), SE(-1,1);
+
+    private int deltaRow;
+    private int deltaColmun;
+
+    /**
+     * Initialize direction values
+     * @param deltaR
+     * @param deltaC 
+     */
+    private Direction(int deltaR, int deltaC) {
+        this.deltaRow = deltaR;
+        this.deltaColmun = deltaC;
+    }
+
+    /**
+     * give deltaR
+     * @return deltaRow
+     */
+    public int getDeltaRow() {
+        return deltaRow;
+    }
+    
+    /**
+     * give deltaC
+     * @return deltaColumn
+     */
+    public int getDeltaColmun() {
+        return deltaColmun;
+    }
+    
+    
+    
+    
+}
