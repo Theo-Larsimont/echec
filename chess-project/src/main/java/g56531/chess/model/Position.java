adf55d0d6034d0c5bd44fdb7bd26483d1c0db165
56531 <56531@etu.he2b.be>
2022-03-15 13:33:57 +0100

    Créer la classe position

---
+++ chess-project/src/main/java/g56531/chess/model/Position.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package g56531.chess.model;
+
+/**
+ * Indicates the position on the game board
+ * @author larsi
+ */
+public class Position {
+    private int row;
+    private int column;
+
+/**
+ * position on the chess board
+ * @param row of tab chess
+ * @param column of tab chess
+ */
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    /**
+     * give the row 
+     * @return row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * give the column
+     * @return column
+     */
+    public int getColumn() {
+        return column;
+    }
+    
+    
+    
+    
+}
