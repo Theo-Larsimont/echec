5d930fa995d49803a1bda67841c10278d5992aba
56531 <56531@etu.he2b.be>
2022-03-18 15:47:59 +0100

    Ajout de Equals et Hashcode

--- chess-project/src/main/java/g56531/chess/model/Position.java
+++ chess-project/src/main/java/g56531/chess/model/Position.java
@@ -37,6 +37,33 @@ public class Position {
     public int getColumn() {
         return this.column;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.row;
+        hash = 89 * hash + this.column;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Position other = (Position) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        return this.column == other.column;
+    }
+    
     
     /**
      * Returns a new position according to the requested direction.
