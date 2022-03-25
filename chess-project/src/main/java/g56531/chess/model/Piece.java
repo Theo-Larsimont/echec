77aaaa6aad7623ebd1f20a6bef6c70137ccddb4f
56531 <56531@etu.he2b.be>
2022-03-25 13:09:31 +0100

    Créer et tester la méthode getPosibleMoves

--- chess-project/src/main/java/g56531/chess/model/Piece.java
+++ chess-project/src/main/java/g56531/chess/model/Piece.java
@@ -4,6 +4,9 @@
  */
 package g56531.chess.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Game piece.
  * @author larsi
@@ -26,6 +29,64 @@ public class Piece {
     public Color getColor() {
         return color;
     }
+    
+    /**
+     * look at all the movement of a possible piece
+     * @param position of piece
+     * @param board
+     * @return 
+     */
+    public List<Position> getPossibleMoves(Position position, Board board){
+        List<Position> possibleMoves = new ArrayList <>();
+        // check if the piece is at the top or bottom edge of the board
+        if(position.getRow() == 0 || position.getRow() == 7){
+            return possibleMoves;
+        }
+        if(board.getPiece(position).getColor() == Color.WHITE){
+            Position nextPos = position.next(Direction.N);
+            if(board.isFree(position.next(Direction.N))){
+                possibleMoves.add(nextPos);
+                //if the piece is at the starting position check if the second square is free
+                if(board.getInitialPawnRow(color) == position.getRow()
+                        && board.isFree(nextPos.next(Direction.N))){
+                    possibleMoves.add(nextPos.next(Direction.N));
+                }
+            }
+            //check if there is an opposing piece to the NW
+            if(position.getColumn() != 0 && 
+                    board.containsOppositeColor(position.next(Direction.NW), color.WHITE)){
+                possibleMoves.add(position.next(Direction.NW));
+            }
+            //check if there is an opposing piece to the NE
+            if(position.getColumn() != 7 && 
+                    board.containsOppositeColor(position.next(Direction.NE), color.WHITE)){
+                possibleMoves.add(position.next(Direction.NE));
+            }
+        }else{
+            Position nextPos = position.next(Direction.S);
+            if(board.isFree(position.next(Direction.S))){
+                possibleMoves.add(nextPos);
+                //if the piece is at the starting position check if the second square is free
+                if(board.getInitialPawnRow(color) == position.getRow()
+                        && board.isFree(nextPos.next(Direction.S))){
+                    possibleMoves.add(nextPos.next(Direction.S));
+                }
+            }
+            //check if there is an opposing piece to the SW
+            if(position.getColumn() != 0 &&
+                    board.containsOppositeColor(position.next(Direction.SW), color.BLACK)){
+                possibleMoves.add(position.next(Direction.SW));
+            }
+            //check if there is an opposing piece to the SE
+            if(position.getColumn() != 7 && 
+                    board.containsOppositeColor(position.next(Direction.SE), color.BLACK)){
+                possibleMoves.add(position.next(Direction.SE));
+            }
+            
+        }
+        return possibleMoves;
+    }
+
 
     
    
---
+++ chess-project/src/test/java/g56531/chess/model/PieceTest.java
@@ -0,0 +1,303 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package g56531.chess.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author larsi
+ */
+public class PieceTest {
+
+    Board board;
+
+    public PieceTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+    }
+
+    @Test
+    public void testGetPossibleMovesP() {
+        Position position = new Position(1, 1);
+        Piece piece = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(2, 1),
+                new Position(3, 1)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleMovesInitialPositionWhitePieceButSecondSquareIsNotFree() {
+        Position position = new Position(1, 1);
+        Position position2 = new Position(3, 1);
+        Piece piece = new Piece(Color.WHITE);
+        Piece pieceOpp = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+        board.setPiece(pieceOpp, position2);
+
+        List<Position> expected = List.of(
+                new Position(2, 1)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleMovesInitialPositionBlackPiece() {
+        Position position = new Position(6, 1);
+        Piece piece = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(5, 1),
+                new Position(4, 1)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleMovesInitialPositionBlackPieceButSecondSquareIsNotFree() {
+        Position position = new Position(6, 1);
+        Position position2 = new Position(4, 1);
+        Piece piece = new Piece(Color.BLACK);
+        Piece pieceOpp = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+        board.setPiece(pieceOpp, position2);
+
+        List<Position> expected = List.of(
+                new Position(5, 1)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleMovesFreeAroundWhitePiece() {
+        Position position = new Position(2, 1);
+        Piece piece = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(3, 1)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleMovesFreeAroundBlackPiece() {
+        Position position = new Position(5, 1);
+        Piece piece = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(4, 1)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleWhiteWithOppositeColorInFront() {
+        Position position = new Position(3, 4);
+        Position PositionOpp = new Position(4, 4);
+        Piece piece = new Piece(Color.WHITE);
+        Piece pieceOpp = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+        board.setPiece(pieceOpp, PositionOpp);
+
+        List<Position> expected = new ArrayList<>();
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleBlackWithOppositeColorInFront() {
+        Position position = new Position(5, 1);
+        Position PositionOpp = new Position(4, 1);
+        Piece piece = new Piece(Color.BLACK);
+        Piece pieceOpp = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+        board.setPiece(pieceOpp, PositionOpp);
+
+        List<Position> expected = new ArrayList<>();
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleWhiteInEndOfBoard() {
+        Position position = new Position(7, 4);
+        Piece piece = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+
+        List<Position> expected = new ArrayList<>();
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleBlackInEndOfBoard() {
+        Position position = new Position(0, 1);
+        Piece piece = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+
+        List<Position> expected = new ArrayList<>();
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleWhiteIsWestEdge() {
+        Position position = new Position(3, 0);
+        Piece piece = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(4, 0)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleWhiteIsEastEdge() {
+        Position position = new Position(3, 7);
+        Piece piece = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(4, 7)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleBlackIsWeastEdge() {
+        Position position = new Position(4, 0);
+        Piece piece = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(3, 0)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleBlackIsEastEdge() {
+        Position position = new Position(4, 7);
+        Piece piece = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+
+        List<Position> expected = List.of(
+                new Position(3, 7)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleWhiteWithDiagonalOppositeColor() {
+        Position position = new Position(4, 4);
+        Position position2 = new Position(5, 3);
+        Position position3 = new Position(5, 5);
+        Piece piece = new Piece(Color.WHITE);
+        Piece pieceOpp = new Piece(Color.BLACK);
+        Piece pieceOpp2 = new Piece(Color.BLACK);
+        board.setPiece(piece, position);
+        board.setPiece(pieceOpp, position2);
+        board.setPiece(pieceOpp2, position3);
+
+        List<Position> expected = List.of(
+                new Position(5, 4),
+                new Position(5, 3),
+                new Position(5, 5)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    @Test
+    public void testGetPossibleBlackWithDiagonalOppositeColor() {
+        Position position = new Position(4, 4);
+        Position position2 = new Position(3, 3);
+        Position position3 = new Position(3, 5);
+        Piece piece = new Piece(Color.BLACK);
+        Piece pieceOpp = new Piece(Color.WHITE);
+        Piece pieceOpp2 = new Piece(Color.WHITE);
+        board.setPiece(piece, position);
+        board.setPiece(pieceOpp, position2);
+        board.setPiece(pieceOpp2, position3);
+
+        List<Position> expected = List.of(
+                new Position(3, 4),
+                new Position(3, 3),
+                new Position(3, 5)
+        );
+
+        List<Position> positions = piece.getPossibleMoves(position, board);
+
+        assertEqualsIgnoringOrder(expected, positions);
+    }
+
+    /*
+     *      Permet de tester si deux listes de positions sont identiques à l'ordre
+     *      des éléments prêts. Cette méthode est appelée
+     *      par les méthodes de test.
+     */
+    private void assertEqualsIgnoringOrder(List<Position> expected, List<Position> actual) {
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+        assertTrue(expected.containsAll(actual));
+    }
+
+}
